bff031e42571063f197ddf2c0a4bde98409a4e74
Your Name <you@example.com>
2024-08-29 15:22:03 +0900

    NO_msg

--- mincoding/c/source_with_inputs.sh
+++ mincoding/c/source_with_inputs.sh
@@ -10,10 +10,26 @@ mkdir -p ${prob_number}
 touch ${prob_number}/${prob_number}.c
 touch ${prob_number}/${prob_number}_input.txt
 echo "#include <stdio.h>
-
+#define MAX_LEN 1000
+#define NUMBER_OF_LINE 5
 int main()
 {
-   
+    # FILE *fs;
+    # char str[NUMBER_OF_LINE][MAX_LEN];
+    # int num = 0;
+
+    # fs = fopen("3-1_input.txt", "r");
+    # if (fs == NULL) {
+    #     printf("파일을 열 수 없습니다.\n");
+    #     return 1;
+    # }
+
+    # while (fgets(str[num], MAX_LEN, fs) != NULL) {
+    #     printf("%s", str[num]);
+    #     num++;
+    # }
+
+    # fclose(fs);
     return 0;
 }" >> ${prob_number}/${prob_number}.c
 
