0b724d62660481a097aeb0722b16ea46303a26c0
Your Name <you@example.com>
2024-08-30 17:30:03 +0900

    NO_msg

---
+++ mincoding/18.5/18.5-11/C++/18.5-11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+
+// 입력변수생성
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("18.5-11_input.txt", "r", stdin);
+
+    return 0;
+}
