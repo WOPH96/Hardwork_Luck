07ed1ba73c52132c3d46cd54b116b3fc5e81ff1c
Your Name <you@example.com>
2024-09-03 17:38:56 +0900

    Fatigue

---
+++ programmers/brute_force/Fatigue/C++/Fatigue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//입력변수생성
+
+//입력변수생성
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("Fatigue_input.txt", "r", stdin);
+
+
+    return 0;
+}
