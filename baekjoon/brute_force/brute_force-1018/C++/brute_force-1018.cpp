2f78443a6be71c3de0557178e74015e46f01a1c7
Your Name <you@example.com>
2024-09-04 19:49:15 +0900

    NO_msg

---
+++ baekjoon/brute_force/brute_force-1018/C++/brute_force-1018.cpp
@@ -0,0 +1,18 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int n, m;
+int arr[50][50];
+// 입력변수생성
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("brute_force-1018_input.txt", "r", stdin);
+
+    return 0;
+}
