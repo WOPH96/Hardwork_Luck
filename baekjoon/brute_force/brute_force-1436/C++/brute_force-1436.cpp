90c42823accfffb77f36d8fc759df91350829d9e
Your Name <you@example.com>
2024-09-09 16:42:08 +0900

    종말의 수

--- baekjoon/brute_force/brute_force-1436/C++/brute_force-1436.cpp
+++ baekjoon/brute_force/brute_force-1436/C++/brute_force-1436.cpp
@@ -1,18 +1,29 @@
 // #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//입력변수생성
-
-//입력변수생성
+// 입력변수생성
+int N;
+// 입력변수생성
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    freopen("brute_force-1436_input.txt", "r", stdin);
-
-
+    // std::ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // freopen("brute_force-1436_input.txt", "r", stdin);
+    cin >> N;
+    int cnt = 0;
+    for (long i = 666; i < 100000000; i++)
+    {
+        if (to_string(i).find("666") != string::npos)
+            cnt++;
+        if (cnt == N)
+        {
+            cout << i;
+            return 0;
+        }
+    }
     return 0;
 }
