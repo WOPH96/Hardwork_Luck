61c722a8ed00564429f38f654c3ff8d83a365ed9
Your Name <you@example.com>
2024-09-09 11:10:50 +0900

    스크립트 이식성 개선

--- ctrl_shell_script/link_script.sh
+++ ctrl_shell_script/link_script.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env zsh
-if [ -z "$1" ]; then
-    echo "상위 디렉토리 이름을 입력해주세요"
-    exit 1
-fi
 
-# $1이 존재하는 디렉토리인지 확인하고, 존재하지 않으면 생성
+# target_dir="$1"
+# if [ -z "${target_dir}" ]; then
+#     echo "상위 디렉토리 이름을 입력해주세요"
+#     exit 1
+# fi
 
-mkdir -p "$1" || {
-    echo "디렉토리 생성 실패: $1"
-    exit 1
-}
+# # $1이 존재하는 디렉토리인지 확인하고, 존재하지 않으면 생성
+# mkdir -p "${target_dir}" || {
+#     echo "디렉토리 생성 실패: $1"
+#     exit 1
+# }
 
 
 top_dir=$(git rev-parse --show-toplevel)
 script_dir=${top_dir}/ctrl_shell_script
 
-# 링크 생성
-ln -sf "${script_dir}/wsl_make_problem_dir.sh" "$1/wsl_make_problem_dir.sh" || {
-    echo "링크 생성 실패: wsl_make_problem_dir.sh"
-    exit 1
-}
-ln -sf "${script_dir}/re_link.sh" "$1/re_link.sh" || {
-    echo "링크 생성 실패: re_link.sh"
-    exit 1
-}
+target_dirs=(baekjoon programmers mincoding/problem)
+# target_dirs=(${TARGET_DIRS//,/ })
+# echo ${#target_dirs[@]}
+for dir in ${target_dirs[@]}; do
+    target_dir="${top_dir}/${dir}"
+    
+    # ... (기존 코드에서 target_dir 사용하는 부분)
+    ln -sf "${script_dir}/wsl_make_problem_dir.sh" "${target_dir}/wsl_make_problem_dir.sh" || {
+        echo "링크 생성 실패: wsl_make_problem_dir.sh"
+        exit 1
+    }
+    ln -sf "${script_dir}/re_link.sh" "${target_dir}/re_link.sh" || {
+        echo "링크 생성 실패: re_link.sh"
+        exit 1
+    }
 
-echo "링크 생성 완료"
+    echo "링크 생성 완료"
+    # echo "$target_dir"
+done
+
+# 링크 생성
