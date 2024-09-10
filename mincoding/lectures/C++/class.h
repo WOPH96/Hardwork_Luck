3c6203ef8893988a18d92ece2e43ab96108517d9
Your Name <you@example.com>
2024-09-10 17:20:58 +0900

    class

---
+++ mincoding/lectures/C++/class.h
@@ -0,0 +1,76 @@
+#include <iostream>
+
+typedef struct
+{
+    int x;
+    int y;
+
+} point_t;
+
+template <typename T>
+class Point
+{
+private:
+    Point *result;
+
+public:
+    T x;
+    T y;
+    Point(T _x = 0, T _y = 0)
+        : x(_x), y(_y)
+    {
+    }
+    ~Point()
+    {
+        delete result;
+    }
+    friend std::ostream &operator<<(std::ostream &out, const Point &point)
+    {
+        out << "(" << point.x << "," << point.y << ")";
+        return out;
+    }
+    Point *operator+(const Point &p2)
+    {
+        result = new Point(this->x + p2.x, this->y + p2.y);
+
+        return result;
+    }
+    Point *operator-(const Point &p2)
+    {
+        result = new Point(this->x - p2.x, this->y - p2.y);
+
+        return result;
+    }
+    Point *operator*(const Point &p2)
+    {
+        result = new Point(this->x * p2.x, this->y * p2.y);
+
+        return result;
+    }
+    Point *operator/(const Point &p2)
+    {
+        result = new Point(this->x / p2.x, this->y / p2.y);
+
+        return result;
+    }
+};
+
+// int main()
+// {
+
+//     point_t pt1;
+//     point_t pt2;
+
+//     pt1 = {11, 22};
+//     pt2 = {33, 34};
+
+//     std::cout << pt1.x << pt1.y << std::endl;
+
+//     Point pt3(1, 2);
+
+//     std::cout << pt3;
+
+//     std::cout << *(pt3 + pt3) << *(pt3 - pt3) << *(pt3 * pt3) << *(pt3 / pt3);
+
+//     return 0;
+// }
--- mincoding/lectures/C++/rect_with_class.cpp
+++ mincoding/lectures/C++/rect_with_class.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
-
+#include "class.h"
+template <typename T>
 class Shape
 {
+
 public:
     void draw()
     {
+        std::cout << "도형을 그립니다." << std::endl;
     }
+    virtual ~Shape() {}
+    virtual T get_area() = 0;
 };
 
+template <typename T>
 class Rect : public Shape
 {
+private:
+    Point<T> p1, p2;
+
+public:
+    Rect(T x1, T y1, T x2, T y2)
+        : p1(x1, y1), p2(x2, y2)
+    {
+    }
+    virtual ~Rect()
+    {
+    }
+    virtual T get_area()
+    {
+        // T res;
+        // res = this->p1 - this->p2;
+        return p1.x;
+    }
 };
 
 int main()
 {
-
+    Rect<int> rect(1, 2, 3, 4);
     return 0;
 }
