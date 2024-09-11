a19f24e4306087b8cc3303722d3dc38f5db52d08
Your Name <you@example.com>
2024-09-11 16:46:00 +0900

    오버라이딩

--- mincoding/lectures/C++/overriding.cpp
+++ mincoding/lectures/C++/overriding.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Animal
 {
@@ -44,5 +45,21 @@ int main()
     Cat c1{"나비", 2};
     c1.cry();
 
+    Animal *a1 = &d1; // 업캐스팅 ! 부모의 참조는 하위객체를 참조할 수 있다.
+
+    a1->cry();
+
+    a1 = &c1;
+    a1->cry();
+
+    std::vector<Animal *> animal_vect;
+    animal_vect.push_back(&d1);
+    animal_vect.push_back(&c1);
+    for (int i = 0; i < animal_vect.size(); i++)
+    {
+        animal_vect[i]->cry();
+    }
+    Animal *a3;
+    static_cast<Dog *>(a3)->cry();
     return 0;
 }
