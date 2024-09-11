f8336d74484c207bad5bd3ed34337d93c6831ce4
Your Name <you@example.com>
2024-09-11 14:52:36 +0900

    스크립트 이식성 개선

--- ctrl_shell_script/cpp_wsl_make_source.sh
+++ ctrl_shell_script/cpp_wsl_make_source.sh
@@ -29,18 +29,35 @@ echo "// #include <bits/stdc++.h>
 
 using namespace std;
 
-//입력변수생성
+// 입력변수생성
 
-//입력변수생성
+// 입력변수생성
+
+// 입력, 테스트 출력
+void input();
+void print();
+
+// void sol(){
+// }
+
 int main()
 {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen(\"${prob_number}_input.txt\", \"r\", stdin);
 
+    // 제출 시 주석처리
+    freopen("test_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    print();
+    // sol();
 
     return 0;
-}" >> ${source_path}/${prob_number}.cpp
+}
+
+void input() {}
+void print() {}" >> ${source_path}/${prob_number}.cpp
 # 실수방지를 위한 이어쓰기
 
 #open
