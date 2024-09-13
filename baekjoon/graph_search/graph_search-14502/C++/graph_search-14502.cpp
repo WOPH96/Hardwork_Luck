f6a0499fc93d002968109f4358c9476ae4d830fa
wonphil <ywonphil@gmail.com>
2024-09-14 07:39:56 +0900

    연구소

--- baekjoon/graph_search/graph_search-14502/C++/graph_search-14502.cpp
+++ baekjoon/graph_search/graph_search-14502/C++/graph_search-14502.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,9 +17,10 @@ typedef struct
     int x;
 } Pos_t;
 
-vector<Pos_t> virus;
+queue<Pos_t> virus;
 vector<Pos_t> empty_space;
 
+int max_zero = -1;
 // 입력, 테스트 출력
 void input();
 void print(int (*now_MAP)[9]);
@@ -27,11 +29,44 @@ bool is_inmap(const Pos_t &p)
     return (0 <= p.y && p.y <= n &&
             0 <= p.x && p.x <= m);
 }
-void bfs(Pos_t start, int (*now_MAP)[9], bool (*visited)[9])
+
+void copy_map(int (*now_MAP)[9])
 {
-    queue<Pos_t> q;
-    q.push(start);
-    visited[start.y][start.x] = true;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            now_MAP[i][j] = MAP[i][j];
+        }
+    }
+}
+int get_zero_space(int (*now_MAP)[9])
+{
+    int total_count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            if (now_MAP[i][j] == 0)
+            {
+                total_count++;
+            }
+        }
+    }
+    return total_count;
+}
+void bfs() //, bool (*visited)[9])
+{
+    // 바이러스 좌표 깊은복사
+    queue<Pos_t> q = virus;
+    // q.push(start);
+    // visited[start.y][start.x] = true;
+
+    // 벽 세운 현재 맵 복사
+    int now_MAP[9][9];
+    copy_map(now_MAP);
+
+    // 좌표 이동
     int dx[4]{1, -1, 0, 0};
     int dy[4]{0, 0, 1, -1};
     while (!q.empty())
@@ -41,43 +76,39 @@ void bfs(Pos_t start, int (*now_MAP)[9], bool (*visited)[9])
         for (int i = 0; i < 4; i++)
         {
             Pos_t next = {now.y + dy[i], now.x + dx[i]};
-            if ( // 맵 안에 있어야 하고, 빈공간이어야 하고, 방문한적없어야
+            if ( // 맵 안에 있어야 하고, 빈공간이면
                 is_inmap(next) &&
-                !now_MAP[next.y][next.x] &&
-                !visited[next.y][next.x])
+                !now_MAP[next.y][next.x])
+            // !visited[next.y][next.x])
             {
                 q.push(next);
-                visited[next.y][next.x] = true;
+                // visited[next.y][next.x] = true;
                 now_MAP[next.y][next.x] = 2;
             }
         }
     }
-    print(now_MAP);
+    // if (get_zero_space(now_MAP) > 25)
+    //     print(now_MAP);
+    max_zero = max(max_zero, get_zero_space(now_MAP));
 }
 
-void sol()
+void make_wall(int cnt)
 {
-    int max_zero_space = -1;
-
-    // 초기 맵 상태 복사하기
-    int init_MAP[9][9];
-    bool visited[9][9] = {0};
-    for (int i = 0; i < n; i++)
+    // 바이러스 퍼뜨리기
+    if (cnt == 3)
     {
-        for (int j = 0; j < m; j++)
+        bfs();
+        return;
+    }
+    for (auto &empty_pos : empty_space)
+    {
+        if (MAP[empty_pos.y][empty_pos.x] == 0)
         {
-            init_MAP[i][j] = MAP[i][j];
+            MAP[empty_pos.y][empty_pos.x] = 1;
+            make_wall(cnt + 1);
+            MAP[empty_pos.y][empty_pos.x] = 0;
         }
     }
-    // 벽 세우기
-
-    // 바이러스 퍼뜨리기
-    for (auto &virus_pos : virus)
-    {
-        bfs(virus_pos, init_MAP, visited);
-    }
-
-    // 현재 맵에서 0 카운트해서, 최댓값 구하기
 }
 
 int main()
@@ -91,8 +122,8 @@ int main()
 
     input();
     // print(MAP);
-    sol();
-
+    make_wall(0);
+    cout << max_zero << endl;
     return 0;
 }
 
@@ -105,7 +136,7 @@ void input()
         {
             cin >> MAP[i][j];
             if (MAP[i][j] == 2)
-                virus.push_back({i, j});
+                virus.push({i, j});
             else if (MAP[i][j] == 0)
                 empty_space.push_back({i, j});
         }
