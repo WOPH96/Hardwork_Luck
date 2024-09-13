5abc192e187bcb117f4395f35fdc2f117808e5d3
wonphil <ywonphil@gmail.com>
2024-09-13 21:03:31 +0900

    link

--- mincoding/problem/02.5/cpp_make_source.sh
+++ mincoding/problem/02.5/cpp_make_source.sh
@@ -1,1 +1,1 @@
-/home/woph/Hardwork_Luck/ctrl_shell_script/cpp_make_source.sh
+/Users/yang-wonphil/Hyundai_Autoever/Hardwork_Luck/ctrl_shell_script/cpp_make_source.sh
--- mincoding/problem/re_link.sh
+++ mincoding/problem/re_link.sh
@@ -1,1 +1,1 @@
-/home/woph/Hardwork_Luck/ctrl_shell_script/re_link.sh
+/Users/yang-wonphil/Hyundai_Autoever/Hardwork_Luck/ctrl_shell_script/re_link.sh
