3892488f6bb73f96a84fc92e44edbc963688b951
wonphil <ywonphil@gmail.com>
2024-09-24 21:13:44 +0900

    알파벳

--- baekjoon/graph_search/graph_search-1987/C++/graph_search-1987.cpp
+++ baekjoon/graph_search/graph_search-1987/C++/graph_search-1987.cpp
@@ -1,7 +1,6 @@
 // #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
@@ -10,7 +9,7 @@ int r, c;
 char graph[21][21] = {0};
 // 다른 변수 생성
 int max_dist = -1;
-vector<char> alphas;
+bool visited[26] = {false};
 
 struct Node
 {
@@ -30,40 +29,32 @@ bool is_in_graph(int y, int x)
     return (0 <= y && y < r &&
             0 <= x && x < c);
 }
-bool is_seen_alpha(const char &alpha)
+bool is_visited(const char &alpha)
 {
-    if (*find(alphas.begin(), alphas.end(), alpha))
-        return true;
-    else
-        return false;
+    return visited[alpha - 'A'];
+}
+
+void dfs(Node now)
+{
+    max_dist = max(max_dist, now.dist);
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = now.y + dy[i];
+        int nx = now.x + dx[i];
+        if (!is_in_graph(ny, nx))
+            continue;
+        if (!is_visited(graph[ny][nx]))
+        {
+            visited[graph[ny][nx] - 'A'] = true;
+            dfs(Node(ny, nx, now.dist + 1));
+            visited[graph[ny][nx] - 'A'] = false;
+        }
+    }
 }
 void sol()
 {
-    stack<Node> s;
-    s.push(Node(0, 0, 1));
-    alphas.push_back(graph[0][0]);
-
-    while (!s.empty())
-    {
-        Node now = s.top();
-        s.pop();
-        max_dist = max(now.dist, max_dist);
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = now.y + dy[i];
-            int nx = now.x + dx[i];
-            if (!is_in_graph(ny, nx))
-                continue;
-
-            if (!is_seen_alpha(graph[ny][nx]))
-            {
-                cout << graph[ny][nx] << " ";
-                s.push(Node(ny, nx, now.dist + 1));
-                alphas.push_back(graph[ny][nx]);
-            }
-        }
-    }
-
+    visited[graph[0][0] - 'A'] = true;
+    dfs(Node(0, 0, 1));
     cout << max_dist << endl;
 }
 
@@ -77,7 +68,7 @@ int main()
     // 제출 시 주석처리
 
     input();
-    print();
+    // print();
     sol();
 
     return 0;
