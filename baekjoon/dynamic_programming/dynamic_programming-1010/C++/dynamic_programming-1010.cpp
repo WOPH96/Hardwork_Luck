4a098a1d8a2c516a2bf5a997ae5bad5dca63b1ac
wonphil <ywonphil@gmail.com>
2024-09-24 19:53:11 +0900

    1010이어서

---
+++ baekjoon/dynamic_programming/dynamic_programming-1010/C++/dynamic_programming-1010.cpp
@@ -0,0 +1,46 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int t;
+// 다른 변수 생성
+
+// 입력, 테스트 출력
+void input();
+void print();
+
+void sol(int n, int m)
+{
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 제출 시 주석처리
+    freopen("dynamic_programming-1010_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    print();
+    // sol();
+
+    return 0;
+}
+
+void input()
+{
+    cin >> t;
+    while (t)
+    {
+        int n, m;
+        cin >> n >> m;
+        sol(n, m);
+        t--;
+    }
+}
+void print() {}
