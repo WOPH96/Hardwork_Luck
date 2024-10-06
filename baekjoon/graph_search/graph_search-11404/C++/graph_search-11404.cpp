097c609c759969853ae6868b94dd065360e71de6
wonphil <ywonphil@gmail.com>
2024-10-06 10:32:46 +0900

    플로이드와샬

--- baekjoon/graph_search/graph_search-11404/C++/graph_search-11404.cpp
+++ baekjoon/graph_search/graph_search-11404/C++/graph_search-11404.cpp
@@ -1,7 +1,8 @@
 // #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
-#define INF (999999)
+#include <iomanip>
+#define INF (999999999)
 using namespace std;
 
 // 입력변수생성
@@ -11,29 +12,44 @@ int way[101][101] = {0};
 // 입력, 테스트 출력
 void print();
 
-// void sol(){
-// }
+void sol()
+{
+    // 맨위에 k가 오는 것이 중요함..
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+
+                way[i][j] = min(way[i][k] + way[k][j], way[i][j]);
+            }
+        }
+    }
+}
 void way_init()
 {
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; i <= n; j++)
+        for (int j = 1; j <= n; j++)
         {
             if (i == j)
                 way[i][j] = 0;
             else
-                way[i][j] = 1000000;
+                way[i][j] = INF;
         }
     }
 }
 void input()
 {
     cin >> n >> m;
+
     way_init();
     for (int i = 0; i < m; i++)
     {
         int from, to, cost;
         cin >> from >> to >> cost;
+        way[from][to] = min(way[from][to], cost);
     }
 }
 
@@ -42,15 +58,32 @@ int main()
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     // 제출 시 주석처리
-    freopen("graph_search-11404_input.txt", "r", stdin);
+    // freopen("graph_search-11404_input.txt", "r", stdin);
     // 제출 시 주석처리
 
     input();
+    // print();
+    sol();
     print();
-    // sol();
 
     return 0;
 }
+
+// void print()
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (way[i][j] == INF)
+//                 cout << "INF ";
+//             else
+//                 cout << std::setw(3) << way[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     cout << endl;
+// }
 
 void print()
 {
@@ -58,7 +91,10 @@ void print()
     {
         for (int j = 1; j <= n; j++)
         {
-            cout << way[i][j] << " ";
+            if (way[i][j] == INF)
+                cout << "0 ";
+            else
+                cout << way[i][j] << " ";
         }
         cout << endl;
     }
