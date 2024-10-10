d732feab704e0b4d89b24a92e234a0c7f40dc949
Wonphil Yang <ywonphil@gmail.com>
2024-10-10 13:49:50 +0900

    별찍기 분할정복 이어서

---
+++ baekjoon/divided_conquer/divided_conquer-2447/C++/divided_conquer-2447.cpp
@@ -0,0 +1,46 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int n;
+// 다른 변수 생성
+
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+    cout << "***" << "\n";
+    cout << "* *" << "\n";
+    cout << "***" << "\n";
+}
+
+void input()
+{
+    cin >> n;
+    n /= 3;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    freopen("divided_conquer-2447_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    print();
+    sol();
+
+    return 0;
+}
+
+void print()
+{
+}
