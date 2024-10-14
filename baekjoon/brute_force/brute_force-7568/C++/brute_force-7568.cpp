1763b77b80b2811b641983393e92b246dd07f37b
wonphil <ywonphil@gmail.com>
2024-10-15 00:01:25 +0900

    덩치 다시

--- baekjoon/brute_force/brute_force-7568/C++/brute_force-7568.cpp
+++ baekjoon/brute_force/brute_force-7568/C++/brute_force-7568.cpp
@@ -1,36 +1,89 @@
+
 // #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 // 입력변수생성
 int n;
-class Person
+// 다른 변수 생성
+struct Person
 {
-public:
-    int weight;
-    int height;
+    int x, y, rank;
+    Person(int x, int y) : x(x), y(y) {}
+    friend ostream &operator<<(ostream &out, Person &p)
+    {
+        out << p.x << "," << p.y;
+        return out;
+    }
 };
-// 입력변수생성
+struct cmp
+{
+    bool operator()(Person &p1, Person &p2)
+    {
+
+        return p1.x > p2.x;
+    }
+};
+
+vector<Person> persons;
+
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+    // sort(persons.begin(), persons.end(), cmp());
+    persons[0].rank = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (persons[i].x > persons[j].x)
+            {
+            }
+        }
+    }
+}
+
+void input()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        persons.emplace_back(x, y);
+    }
+}
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
     freopen("brute_force-7568_input.txt", "r", stdin);
-    cin >> n;
-    Person *p = new Person[n]();
-    for (int i = 0; i < n; i++)
-    {
+    // 제출 시 주석처리
 
-        cin >> p[i].weight >> p[i].height;
-    }
+    input();
+    sol();
+    print();
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << p[i].weight << " " << p[i].height << endl;
-    }
-    delete[] p;
     return 0;
+}
+
+void print()
+{
+    // for (auto &p : persons)
+    // {
+    //     cout << p << endl;
+    // }
+    for (auto &p : persons)
+    {
+        cout << p.rank << endl;
+    }
 }
