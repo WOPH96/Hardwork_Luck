d0e451146434cbb813b47fa60d597b3dff571a5e
Wonphil Yang <ywonphil@gmail.com>
2024-10-16 20:36:44 +0900

    집가서무조건풀기

---
+++ baekjoon/bit_masking/bit_masking-14569/C++/bit_masking-14569.cpp
@@ -0,0 +1,42 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int n, m;
+
+// 다른 변수 생성
+
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+}
+
+void input()
+{
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    freopen("bit_masking-14569_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    sol();
+    print();
+
+    return 0;
+}
+
+void print()
+{
+}
