7f54d9bc4072529775311dc0914facf5aa4caf7e
Wonphil Yang <ywonphil@gmail.com>
2024-10-16 20:35:44 +0900

    사격내기

---
+++ baekjoon/bit_masking/bit_masking-27960/C++/bit_masking-27960.cpp
@@ -0,0 +1,44 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int a, b;
+// 다른 변수 생성
+
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+    int res = a ^ b;
+    cout << res << endl;
+}
+
+void input()
+{
+    cin >> a >> b;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    // freopen("bit_masking-27960_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    sol();
+    print();
+
+    return 0;
+}
+
+void print()
+{
+}
