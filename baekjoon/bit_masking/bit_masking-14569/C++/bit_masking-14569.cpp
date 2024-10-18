545914573850d55b15769cb12ae4878f8ed6e98f
Wonphil Yang <ywonphil@gmail.com>
2024-10-18 16:47:32 +0900

    시간표 짜기

--- baekjoon/bit_masking/bit_masking-14569/C++/bit_masking-14569.cpp
+++ baekjoon/bit_masking/bit_masking-14569/C++/bit_masking-14569.cpp
@@ -6,30 +6,55 @@ using namespace std;
 
 // 입력변수생성
 int n, m;
-vector<int> classes[1001];
-vector<int> classes_std[10001];
+int k[1001] = {0};
+int classes[1001][51] = {0};
+int classes_std[10001][51] = {0};
 // 다른 변수 생성
 
 // 입력, 테스트 출력
 
 void print();
-
+int check(int *student)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int correct = 0;
+        for (int j = 1; j <= 50; j++)
+        {
+            if (classes[i][j] && student[j])
+                correct++;
+            if (correct == k[i])
+            {
+                cnt++;
+                break;
+            }
+        }
+    }
+    return cnt;
+}
 void sol()
 {
+    // 수업 1 & 학생시간 1이어야 할때
+    for (int i = 0; i < m; i++)
+    {
+        cout << check(classes_std[i]) << "\n";
+    }
 }
 
 void input()
 {
-    int k, p;
+    int p;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        cin >> k;
-        for (int j = 0; j < k; j++)
+        cin >> k[i];
+
+        for (int j = 0; j < k[i]; j++)
         {
             int time;
             cin >> time;
-            classes[i].push_back(time);
+            classes[i][time] = 1;
         }
     }
     cin >> m;
@@ -40,7 +65,7 @@ void input()
         {
             int time;
             cin >> time;
-            classes_std[i].push_back(time);
+            classes_std[i][time] = 1;
         }
     }
 }
@@ -51,12 +76,12 @@ int main()
     cin.tie(nullptr);
 
     // 제출 시 주석처리
-    freopen("bit_masking-14569_input.txt", "r", stdin);
+    // freopen("bit_masking-14569_input.txt", "r", stdin);
     // 제출 시 주석처리
 
     input();
     sol();
-    print();
+    // print();
 
     return 0;
 }
