2d1255a7d376714118dab2aa4c99889f69c819da
Wonphil Yang <ywonphil@gmail.com>
2024-10-25 17:22:29 +0900

    강의실배정

---
+++ baekjoon/greedy/greedy-11000/C++/greedy-11000.cpp
@@ -0,0 +1,47 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int n;
+
+struct Time
+{
+    int start, end;
+    Time(int s, int e) : start(s), end(e) {}
+};
+// 다른 변수 생성
+vector<Time> lectures;
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+}
+
+void input()
+{
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    freopen("greedy-11000_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    sol();
+    print();
+
+    return 0;
+}
+
+void print()
+{
+}
