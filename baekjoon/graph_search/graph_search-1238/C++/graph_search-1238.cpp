fe87c5a72b4be2690cfcaacdb28f33bb852a0ec3
Wonphil Yang <ywonphil@gmail.com>
2024-10-31 11:26:45 +0900

    다시

---
+++ baekjoon/graph_search/graph_search-1238/C++/graph_search-1238.cpp
@@ -0,0 +1,45 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 입력변수생성
+int n, m, x;
+// 다른 변수 생성
+vector<int> graph[1001];
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+}
+
+void input()
+{
+    cin >> n >> m >> x;
+    for (int i = 0; i < m; i++)
+    {
+        }
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    freopen("graph_search-1238_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    sol();
+    print();
+
+    return 0;
+}
+
+void print()
+{
+}
