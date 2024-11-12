14beda2164ff11308c175eb762f6bca92a146a4b
Wonphil Yang <ywonphil@gmail.com>
2024-11-12 14:45:06 +0900

    인구이동

--- baekjoon/graph_search/graph_search-16234/C++/graph_search-16234.cpp
+++ baekjoon/graph_search/graph_search-16234/C++/graph_search-16234.cpp
@@ -1,6 +1,7 @@
 // #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,10 +12,100 @@ int l, r;
 int graph[51][51] = {0};
 // 입력, 테스트 출력
 
+int dy[4] = {0, 0, -1, 1};
+int dx[4] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int y, x;
+    Pos(int y, int x) : y(y), x(x) {}
+};
+
 void print();
 
+bool is_valid(int y, int x)
+{
+    return (0 <= y && y < n &&
+            0 <= x && x < n);
+}
+
+bool bfs(int y, int x, bool (*visited)[51])
+{
+    if (visited[y][x])
+        return false;
+    queue<Pos> q;
+    vector<Pos> unions;
+
+    q.emplace(y, x);
+    unions.emplace_back(y, x);
+    visited[y][x] = true;
+    while (!q.empty())
+    {
+        Pos now = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = now.y + dy[i];
+            int nx = now.x + dx[i];
+            if (!is_valid(ny, nx))
+                continue;
+            int gap = abs(graph[ny][nx] - graph[now.y][now.x]);
+            // 방문한 적 없고, 차이값이 l과 r 사이라면
+            if (!visited[ny][nx] && (l <= gap && gap <= r))
+            {
+                q.emplace(ny, nx);
+                unions.emplace_back(ny, nx);
+                visited[ny][nx] = true;
+            }
+        }
+    }
+    int union_size = unions.size();
+    if (union_size == 1)
+        return true;
+    int sum = 0;
+    int avg = 0;
+    for (auto &elem : unions)
+    {
+        // cout << elem.y << "," << elem.x << endl;
+        sum += graph[elem.y][elem.x];
+    }
+    avg = sum / unions.size();
+    // cout << "평균" << avg << endl;
+
+    for (auto &elem : unions)
+    {
+        graph[elem.y][elem.x] = avg;
+    }
+    // print();
+    return true;
+}
 void sol()
 {
+    int day = 0;
+    while (true)
+    {
+        int space = 0;
+        bool visited[51][51] = {false};
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (bfs(i, j, visited))
+                {
+                    space++;
+                }
+            }
+        }
+        // 모든 나라가 독립적임
+        if (space == n * n)
+        {
+            // cout << "independent ! " << endl;
+            break;
+        }
+        day++;
+        // break;
+    }
+    cout << day << endl;
 }
 
 void input()
@@ -40,7 +131,7 @@ int main()
 
     input();
     sol();
-    print();
+    // print();
 
     return 0;
 }
