3983d6d1e2b09c58e1e1df1f1c68de5228ea4278
Wonphil Yang <ywonphil@gmail.com>
2024-11-14 19:43:03 +0900

    백조의호수 다시 진짜왜틀린지모르겠음

--- baekjoon/graph_search/graph_search-3197/C++/graph_search-3197.cpp
+++ baekjoon/graph_search/graph_search-3197/C++/graph_search-3197.cpp
@@ -8,7 +8,9 @@ using namespace std;
 // 입력변수생성
 int n, m;
 char prev_graph[1501][1501] = {0};
-char next_graph[1501][1501] = {0};
+bool visited_sw1[1501][1501] = {false};
+bool visited_sw2[1501][1501] = {false};
+// char next_graph[1501][1501] = {0};
 // char empty_graph[100][1501][1501] = {0};
 // 다른 변수 생성
 int dy[4] = {0, 0, 1, -1};
@@ -33,9 +35,11 @@ struct Swans
 
 Swans sw;
 
+queue<Pos> water_q;
+
 // 입력, 테스트 출력
 
-void print(char (*prev)[1501], char (*next)[1501]);
+void print(char (*prev)[1501]); //, char (*next)[1501]);
 
 #define WATER '.'
 #define ICE 'X'
@@ -46,112 +50,159 @@ bool is_valid(int y, int x)
     return (y >= 0 && y < n && x >= 0 && x < m);
 }
 
-void melting(char (*prev)[1501], char (*next)[1501])
+// void melting(char (*prev)[1501], char (*next)[1501])
+// {
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             next[i][j] = prev[i][j];
+//             if (prev[i][j] == WATER)
+//             {
+//                 continue;
+//             }
+//             else
+//             {
+//                 // cout << i << "," << j << ":" << prev[i][j] << endl;
+
+//                 for (int k = 0; k < 4; k++)
+//                 {
+//                     int ny = i + dy[k];
+//                     int nx = j + dx[k];
+//                     if (!is_valid(ny, nx))
+//                     {
+//                         continue;
+//                     }
+//                     if (prev[ny][nx] == WATER && next[i][j] == ICE)
+//                     {
+
+//                         next[i][j] = WATER;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+void melting(char (*prev)[1501], queue<Pos> &water_queue)
 {
-
-    for (int i = 0; i < n; i++)
+    queue<Pos> new_water_queue;
+    vector<Pos> postemp;
+    while (!water_queue.empty())
     {
-        for (int j = 0; j < m; j++)
+        Pos water = water_queue.front();
+        water_queue.pop();
+        for (int i = 0; i < 4; i++)
         {
-            next[i][j] = prev[i][j];
-            if (prev[i][j] == WATER)
-            {
+            int ny = water.y + dy[i];
+            int nx = water.x + dx[i];
+            if (!is_valid(ny, nx))
                 continue;
-            }
-            else
+
+            if (prev[ny][nx] == ICE)
             {
-                // cout << i << "," << j << ":" << prev[i][j] << endl;
-
-                for (int k = 0; k < 4; k++)
-                {
-                    int ny = i + dy[k];
-                    int nx = j + dx[k];
-                    if (!is_valid(ny, nx))
-                    {
-                        continue;
-                    }
-                    if (prev[ny][nx] == WATER && next[i][j] == ICE)
-                    {
-
-                        next[i][j] = WATER;
-                    }
-                }
+                // next[ny][nx] = WATER;
+                new_water_queue.emplace(ny, nx);
+                postemp.emplace_back(ny, nx);
             }
         }
     }
+    for (Pos waterpos : postemp)
+    {
+        prev[waterpos.y][waterpos.x] = WATER;
+    }
+
+    swap(water_queue, new_water_queue);
 }
 
-bool bfs(char (*ice)[1501], bool (*visited)[1501])
+bool bfs(char (*ice)[1501])
 {
-    queue<Pos> q;
 
-    q.push(sw.start);
-    visited[sw.start.y][sw.start.x] = true;
-    while (!q.empty())
+    queue<Pos> q_sw1;
+    queue<Pos> q_sw2;
+
+    q_sw1.push(sw.start);
+    visited_sw1[sw.start.y][sw.start.x] = true;
+    q_sw2.push(sw.target);
+    visited_sw2[sw.target.y][sw.target.x] = true;
+    while (!q_sw1.empty() || !q_sw2.empty())
     {
-        Pos now = q.front();
-        q.pop();
-        if (now.y == sw.target.y && now.x == sw.target.x)
+        if (!q_sw1.empty())
         {
-            // cout << "GOT IT !" << endl;
-            return true;
-        }
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = now.y + dy[i];
-            int nx = now.x + dx[i];
-
-            if (!is_valid(ny, nx))
-                continue;
-            if (!visited[ny][nx] && ice[ny][nx] != ICE)
+            Pos now = q_sw1.front();
+            q_sw1.pop();
+            if (visited_sw2[now.y][now.x])
             {
-                q.emplace(ny, nx);
-                visited[ny][nx] = true;
+                // 다른 백조가 간 지점 방문했으면
+                // cout << "GOT IT !" << endl;
+                return true;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+
+                if (!is_valid(ny, nx))
+                    continue;
+                if (!visited_sw1[ny][nx] && ice[ny][nx] != ICE)
+                {
+                    q_sw1.emplace(ny, nx);
+                    visited_sw1[ny][nx] = true;
+                }
+            }
+        }
+        if (!q_sw2.empty())
+        {
+            Pos now = q_sw2.front();
+            q_sw2.pop();
+            if (visited_sw1[now.y][now.x])
+            {
+                // 다른 백조가 간 지점 방문했으면
+                // cout << "GOT IT !" << endl;
+                return true;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+
+                if (!is_valid(ny, nx))
+                    continue;
+                if (!visited_sw2[ny][nx] && ice[ny][nx] != ICE)
+                {
+                    q_sw2.emplace(ny, nx);
+                    visited_sw2[ny][nx] = true;
+                }
             }
         }
     }
     return false;
 }
-void sol(char (*prev)[1501], char (*next)[1501])
+void sol(char (*prev)[1501]) //, char (*next)[1501])
 {
     char(*prev_ice)[1501] = prev;
-    char(*next_ice)[1501] = next;
+    // char(*next_ice)[1501] = next;
     char(*temp)[1501];
-    int day = 0;
+    int day = 1;
     // bool meet_success = false;
     while (true)
     {
-
+        melting(prev_ice, water_q);
+        fill(&visited_sw1[0][0], &visited_sw1[n + 1][m + 1], false);
+        fill(&visited_sw2[0][0], &visited_sw2[n + 1][m + 1], false);
         // PREV가 원하는 배열
-        bool visited[1501][1501] = {false};
-
         // 백조가 만날 수 있다면 break
-        if (bfs(prev_ice, visited))
+        if (bfs(prev_ice))
         {
             // meet_success = true;
             break;
         }
 
         // 빙산 녹이기
-        melting(prev_ice, next_ice);
-        // print(prev_ice, next_ice);
-
-        // SWAP
-
-        temp = prev_ice;
-        prev_ice = next_ice;
-        next_ice = temp;
-        // 더 녹여야 한다면,
-        // next 초기화
-        // for (int i = 0; i < n; i++)
-        // {
-        //     for (int j = 0; j < m; j++)
-        //     {
-        //         next_ice[i][j] = 0;
-        //     }
-        // }
 
         day++;
+        // cout << "DAY" << day << endl;
+        // print(prev_ice);
     }
     cout << day << endl;
 }
@@ -175,6 +226,11 @@ void input()
                 else
                     sw.target = {i, j};
                 num_swan++;
+                water_q.emplace(i, j);
+            }
+            else if (prev_graph[i][j] == WATER)
+            {
+                water_q.emplace(i, j);
             }
         }
     }
@@ -190,15 +246,15 @@ int main()
     // 제출 시 주석처리
 
     input();
-    sol(prev_graph, next_graph);
+    sol(prev_graph); //, next_graph);
     // print();
 
     return 0;
 }
 
-void print(char (*prev)[1501], char (*next)[1501])
+void print(char (*prev)[1501]) //, char (*next)[1501])
 {
-    cout << sw.start << " " << sw.target << endl;
+    // cout << sw.start << " " << sw.target << endl;
     cout << "================" << endl;
     for (int i = 0; i < n; i++)
     {
@@ -209,14 +265,14 @@ void print(char (*prev)[1501], char (*next)[1501])
         cout << endl;
     }
     cout << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << next[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << next[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
     cout << "================" << endl;
 }
