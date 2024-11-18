878680667b92c6515409f951ad0add3abf2c2623
wonphil <ywonphil@gmail.com>
2024-11-19 00:32:59 +0900

    거스름돈

---
+++ baekjoon/greedy/greedy-2170/C++/greedy-2170.cpp
@@ -0,0 +1,47 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+// 입력변수생성
+int n;
+struct Node
+{
+    ll from, to;
+    Node(int f, int t) : from(f), to(t) {}
+};
+// 다른 변수 생성
+
+// 입력, 테스트 출력
+
+void print();
+
+void sol()
+{
+}
+
+void input()
+{
+    cin >> n;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 제출 시 주석처리
+    freopen("greedy-2170_input.txt", "r", stdin);
+    // 제출 시 주석처리
+
+    input();
+    sol();
+    print();
+
+    return 0;
+}
+
+void print()
+{
+}
