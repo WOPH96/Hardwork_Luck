3cfa87064b104d7b6fba0fae2e08debb16f59225
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-04-23 20:37:50 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 68ms, Memory: 29MB, Status: Passed - Codetree

--- 250423/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.c
+++ 250423/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.c
@@ -18,52 +18,37 @@ int is_valid(int y,int x)
 
 int visited[501][501];
 
-void bfs(Pos start)
+int dy[] = {0,0,1,-1};
+int dx[] = {1,-1,0,0};
+
+int dfs(Pos now)
 {
-    if(visited[start.y][start.x]) return;
-    // start.visit_cnt = 1;
-    visited[start.y][start.x] = 1;
-    Pos queue[5000*5000];
-    int op =0, ip =0;
-    queue[ip++] = start;
-    int dy[] = {0,0,1,-1};
-    int dx[] = {1,-1,0,0};
+    if(visited[now.y][now.x]) return visited[now.y][now.x];
+    // now.visit_cnt = 1;
+    visited[now.y][now.x] = 1;
+    // Pos queue[500*500];
+    // int op =0, ip =0;
+    // queue[ip++] = now;
+
     
     // int cnt = 1;
-    // printf("In BFS start from %d\n",grid[start.y][start.x]);
-    while(ip>op)
+    // printf("In BFS now from %d\n",grid[now.y][now.x]);
+
+    for(int i = 0; i<4;i++)
     {
-        Pos now = queue[op++];
-        for(int i = 0; i<4;i++)
+        int ny = now.y+dy[i], nx = now.x+dx[i];
+        if(is_valid(ny,nx) &&
+            grid[ny][nx] > grid[now.y][now.x])
         {
-            int ny = now.y+dy[i], nx = now.x+dx[i];
-            if(is_valid(ny,nx) && visited[ny][nx]< now.visit_cnt+1 &&
-                grid[ny][nx] > grid[now.y][now.x])
-            {
-                // printf("[%d,%d] = [%d]\n",ny,nx,grid[ny][nx]);
-                visited[ny][nx] = now.visit_cnt+1;
-                queue[ip++] = (Pos){ny,nx,now.visit_cnt+1};
-                // cnt++;
-            }
+            int res = dfs((Pos){ny,nx}) + 1;
+            if(visited[now.y][now.x] < res) visited[now.y][now.x] = res;
         }
     }
+    return visited[now.y][now.x];
+
     // return cnt;
 }
-void print_visit(){
-    for(int i = 0; i<n; i++)
-    {
-        for (int j =0; j<n; j++)
-        {
-            // max_res = max(max_res,visited[i][j]);
-            printf("%d ",visited[i][j]);
-            // printf("[%d,%d] = %d\n",i,j,res);
-            // max_res = max(max_res,res);
-        }
-        printf("\n");
-        
-    }
-    printf("\n");
-}
+
 int main() {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
@@ -75,26 +60,26 @@ int main() {
     // Please write your code here.
 
     
-
+    int max_res = -1;
     for(int i = 0; i<n; i++)
     {
         for (int j =0; j<n; j++)
         {
             // printf("[%d,%d] = %d\n",i,j,grid[i][j]);
-            bfs((Pos){i,j,1});
+            int res = dfs((Pos){i,j});
             // print_visit();
             
-            // max_res = max(max_res,res);
+            max_res = max(max_res,res);
         }
     }    
-    int max_res = -1;
-    for(int i =0; i<n;i++)
-    {
-        for(int j =0; j<n;j++)
-        {
-            max_res = max(max_res,visited[i][j]);
-        }
-    }
+
+    // for(int i =0; i<n;i++)
+    // {
+    //     for(int j =0; j<n;j++)
+    //     {
+    //         max_res = max(max_res,visited[i][j]);
+    //     }
+    // }
     printf("%d",max_res);
         
     // printf("%d\n",max_res);
