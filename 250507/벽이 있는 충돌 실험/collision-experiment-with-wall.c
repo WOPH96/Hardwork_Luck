582afc71a166a864e548d9b3564c4b2a5add1d98
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-07 16:59:27 +0900

    Title: 벽이 있는 충돌 실험, Time: 164ms, Memory: 0MB, Status: Passed - Codetree

--- 250507/벽이 있는 충돌 실험/collision-experiment-with-wall.c
+++ 250507/벽이 있는 충돌 실험/collision-experiment-with-wall.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #define MAX_M 2500
 
 int T;
@@ -30,108 +29,81 @@ void init_dir()
     direction['L'] = (Pos){0,-1};
 }
 
-void print(int grid[][50])
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            printf("%d ",grid[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-void move(Marble *marble,int visited[][50])
+void move(Marble *marble)
 {
     if(!marble->alive) return;
 
     Pos now = marble->curr_pos;
     int ny = now.y + marble->dir.y, nx = now.x + marble->dir.x;
+
     if(is_valid(ny,nx))
     {
-        visited[now.y][now.x]--;
-        visited[ny][nx]++;
         marble->curr_pos = (Pos){ny,nx};
     }
     else
     {
-        //방향전환
+        // 방향 전환
         marble->dir.y *= -1;
         marble->dir.x *= -1;
     }
 }
 
-void check_and_remove(int visited[][50])
+// 여기부터 개선된 check_and_remove!
+void check_and_remove_fast()
 {
-    for(int i =0 ;i<n; i++)
-    {
-        for(int j =0; j<n;j++)
-        {
-            if(visited[i][j] >=2)
-            {
-                visited[i][j] = 0;
-                for(int k = 0; k<m;k++)
-                {
-                    if(marble[k].curr_pos.y == i && 
-                        marble[k].curr_pos.x == j )
-                    {
-                        marble[k].alive =0;
-                        
-                    }
-                }
+    int cell_cnt[50][50] = {0}; // 각 칸에 구슬이 몇 개 있는지 카운트
 
-            }
+    // 1. 살아있는 구슬들 위치 카운트
+    for(int i = 0; i < m; i++)
+    {
+        if(marble[i].alive) {
+            int y = marble[i].curr_pos.y;
+            int x = marble[i].curr_pos.x;
+            cell_cnt[y][x]++;
+        }
+    }
+    // 2. 겹치는 칸의 구슬 모두 죽이기
+    for(int i = 0; i < m; i++)
+    {
+        if(marble[i].alive) {
+            int y = marble[i].curr_pos.y;
+            int x = marble[i].curr_pos.x;
+            if(cell_cnt[y][x] >= 2)
+                marble[i].alive = 0;
         }
     }
 }
+
 
 int main() {
     init_dir();
     scanf("%d", &T);
     while (T--) {
         scanf("%d %d", &n, &m);
-        int visited[50][50] = {0};
+
         for (int i = 0; i < m; i++) {
             char c;
             scanf("%d %d %c", &marble[i].curr_pos.y, &marble[i].curr_pos.x, &c);
             marble[i].curr_pos.y--, marble[i].curr_pos.x--;
             marble[i].alive = 1;
             marble[i].dir = direction[c];
-            visited[marble[i].curr_pos.y][marble[i].curr_pos.x] = 1;
-        }
-        // Please write your code here.
-        // for (int i = 0; i < m; i++) {
-        //     printf("%d번째 curr[%d,%d], dir[%d,%d], alive : %d\n",
-        //             i+1,marble[i].curr_pos.y,marble[i].curr_pos.x,
-        //             marble[i].dir.y,marble[i].dir.x,marble[i].alive);
-        // }
-        
-        for(int time = 0; time <2*n; time++)
-        {
-            for(int i = 0; i<m; i++)
-            {
-                move(&marble[i],visited);
-                
-            }
-            // print(visited);
-            check_and_remove(visited);
-            // break;
         }
 
+        for(int time = 0; time < 2 * n; time++)
+        {
+            // 모든 구슬 이동
+            for(int i = 0; i < m; i++)
+                move(&marble[i]);
+            // 겹치는 구슬 제거 (성능 개선 버전 사용)
+            check_and_remove_fast();
+        }
+
+        // 살아있는 구슬 count
         int cnt = 0;
-        for(int i =0; i<n; i++)
-        {
-            for(int j =0; j<n; j++)
-            {
-                if(visited[i][j] == 1)
-                cnt++;
-            }
-        }
-        printf("%d\n",cnt);
+        for (int i = 0; i < m; i++)
+            if (marble[i].alive) cnt++;
 
-
+        printf("%d\n", cnt);
     }
     return 0;
 }
