295d338f6433d793b833bf877bb272ce9bc3cbcf
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-12 13:13:01 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250512/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.c
+++ 250512/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.c
@@ -17,7 +17,12 @@ int cmp(const void *a, const void *b)
     Line *src = (Line*)a;
     Line *dst = (Line*)b;
 
+    if(src->end == dst->end)
+    {
+        return src->start > dst->start;
+    }
     return src->end > dst->end;
+    
 }
 
 int main() {
@@ -29,10 +34,29 @@ int main() {
 
     qsort(segments,n,sizeof(Line),cmp);
 
+    int mem[1000] = {0};
+    int idx = 1;
     for(int i =0; i<n; i++)
     {
         // printf("%d %d\n",segments[i].start,segments[i].end);
-    }
+        if(mem[idx-1] < segments[i].start)
+        {
+            // printf("mem[%d] : %d segment->[%d,%d]\n",
+            //         idx,mem[idx],segments[i].start,segments[i].end);
+            mem[idx++] = segments[i].end;
+            
+        }
 
+    }
+    printf("%d",idx-1);
+    // for(int i =0; i<idx; i++)
+    // {
+    //     printf("%d\n",mem[i]);
+    //     // if(mem[idx] < segments[i].start)
+    //     // {
+    //     //     mem[idx++] = segments[i].end;
+    //     // }
+
+    // }
     return 0;
 }
