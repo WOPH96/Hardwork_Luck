d413f03243b089fe4138487d44e2c9922bdb345a
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-16 11:19:16 +0900

    Title: 부분 수열의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250516/부분 수열의 합/the-sum-of-the-subsequences.c
+++ 250516/부분 수열의 합/the-sum-of-the-subsequences.c
@@ -3,6 +3,24 @@
 int n, m;
 int arr[101];
 
+int res = 0;
+void backtrack(int sum, int idx)
+{
+    if(sum == m)
+    {
+        res = 1;
+        return;
+    }
+    if(idx >= n || sum > m || res) return;
+
+    //선택
+    backtrack(sum+arr[idx],idx+1);
+
+    //미선택
+    backtrack(sum,idx+1);
+    
+}
+
 int main() {
     scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++) {
@@ -11,26 +29,29 @@ int main() {
     
     // Please write your code here.
     
-    int dp[10001] = {0};
-    for(int i = 1; i<=m;i++)
-    {
-        dp[i] = -1;
-    }
-    //dp[i] = i를 만들 수 있으면 1 업승면 0
+    // int dp[10001] = {0};
+    // for(int i = 1; i<=m;i++)
+    // {
+    //     dp[i] = -1;
+    // }
+    // //dp[i] = i를 만들 수 있으면 1 업승면 0
 
-    for(int i = 0; i<n; i++)
-    {
-        for(int j =m; j>=1; j--)
-        { 
-            if(j >= arr[i] && dp[j - arr[i]] != -1 )
-            {
-                dp[j] = 1;
-            }
-        }
-    }
+    // for(int i = 0; i<n; i++)
+    // {
+    //     for(int j =m; j>=1; j--)
+    //     { 
+    //         if(j >= arr[i] && dp[j - arr[i]] != -1 )
+    //         {
+    //             dp[j] = 1;
+    //         }
+    //     }
+    // }
 
-    if(dp[m] == 1) printf("Yes");
-    else printf("No");
+    // if(dp[m] == 1) printf("Yes");
+    // else printf("No");
+    backtrack(0,0);
+    if(res) printf("Yes");
+    else    printf("No");
 
     return 0;
 }
