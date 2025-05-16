0917734da9dcabca234e768d08f34a1ded80f82b
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-16 09:37:46 +0900

    Title: 부분 수열의 합이 M, Time: 50ms, Memory: 2MB, Status: Passed - Codetree

--- 250516/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.c
+++ 250516/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.c
@@ -7,75 +7,81 @@ int arr[100];
 int min_length = MAX_VAL;
 #define min(a,b) ((a<b)?a:b)
 
-// void backtrack(int sum, int idx, int cnt)
-// {
-//     if(sum == m)
-//     {
-//         min_length = min(cnt,min_length);
-//         return;
-//     }
+int visited[10001][101];
 
-//     if(idx>=n) return;
-//     if(sum > m || cnt >= min_length) return;
-
-//     //현재 요소 선택했을때
-//     backtrack(sum+arr[idx],idx+1,cnt+1);
-
-//     //현재 요소 선택x
-//     backtrack(sum,idx+1,cnt);
-// }
-
-// int main() {
-//     scanf("%d %d", &n, &m);
-//     for (int i = 0; i < n; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-    
-//     // Please write your code here.
-    
-//     // 합이 12가 될때 리턴하도록 
-
-//     backtrack(0,0,0);
-
-//     if(min_length == MAX_VAL)
-//         printf("-1");
-//     else
-//         printf("%d",min_length);
-
-//     return 0;
-// }
-
-int main() 
+void backtrack(int sum, int idx, int cnt)
 {
+    if(sum == m)
+    {
+        min_length = min(cnt,min_length);
+        return;
+    }
+
+    if(idx>=n) return;
+    if(sum > m || cnt >= min_length) return;
+
+    // 이미 이 상태(sum, idx)는 더 좋은 cnt로 방문한 적이 있으면 return
+    if (visited[sum][idx] && visited[sum][idx] <= cnt) return;
+    visited[sum][idx] = cnt;
+
+    //현재 요소 선택했을때
+    backtrack(sum+arr[idx],idx+1,cnt+1);
+
+    //현재 요소 선택x
+    backtrack(sum,idx+1,cnt);
+}
+
+int main() {
     scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    
+    // Please write your code here.
+    
+    // 합이 12가 될때 리턴하도록 
 
-    //dp[i] -> 지금까지 선택한 수들의 합이 i일때, 가능한 최소 수열의 길이
+    backtrack(0,0,0);
 
-
-    int dp[10001] = {0};
-
-    for(int i =1;                  i<=m;i++)
-    {
-        dp[i]= MAX_VAL;
-    }
-
-    //dp[5, 1, 20, 3, 4] -> 1
-    //dp[6 21 ]
-
-    for(int i=0; i<n; i++)
-    {
-        for(int j=m;j>=0;j--)
-        {
-            if(j>= arr[i])
-                dp[j] = min(dp[j],dp[j - arr[i]] +1);
-        }
-    }
-
-    if(dp[m] == MAX_VAL)
+    if(min_length == MAX_VAL)
         printf("-1");
     else
-        printf("%d",dp[m]);
+        printf("%d",min_length);
+
+    return 0;
 }
+
+// int main() 
+// {
+//     scanf("%d %d", &n, &m);
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     //dp[i] -> 지금까지 선택한 수들의 합이 i일때, 가능한 최소 수열의 길이
+
+
+//     int dp[10001] = {0};
+
+//     for(int i =1;                  i<=m;i++)
+//     {
+//         dp[i]= MAX_VAL;
+//     }
+
+//     //dp[5, 1, 20, 3, 4] -> 1
+//     //dp[6 21 ]
+
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=m;j>=0;j--)
+//         {
+//             if(j>= arr[i])
+//                 dp[j] = min(dp[j],dp[j - arr[i]] +1);
+//         }
+//     }
+
+//     if(dp[m] == MAX_VAL)
+//         printf("-1");
+//     else
+//         printf("%d",dp[m]);
+// }
