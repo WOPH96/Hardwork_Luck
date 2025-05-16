83973e0c8f0d2069e0987f1556fd948624b4c03f
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-16 15:19:38 +0900

    Title: 배낭 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250516/배낭 채우기/knapsack.c
+++ 250516/배낭 채우기/knapsack.c
@@ -29,7 +29,7 @@ void print()
 int max_value = -1;
 
 // int max_arr[10001][101];
-int max_arr[101];
+int max_arr[10001]; // max_arr[i] -> i 무게에서 최대 벨류 
 
 void backtrack(int idx, int sum_weight, int sum_value)
 {
@@ -39,12 +39,12 @@ void backtrack(int idx, int sum_weight, int sum_value)
         max_value = sum_value;
     if(idx >= N) return;
 
-    if(max_arr[idx] && max_arr[idx] < sum_value) return;
+    if(max_arr[sum_weight] && max_arr[sum_weight] < sum_value) return;
 
     //선택 할 떄
-    max_arr[idx] = sum_value+jews[idx].v;
+    max_arr[sum_weight] = sum_value+jews[idx].v;
     backtrack(idx+1, sum_weight+jews[idx].w, sum_value+jews[idx].v);
-    max_arr[idx] = sum_value;
+    max_arr[sum_weight] = sum_value;
 
     //선택 x
     backtrack(idx+1, sum_weight, sum_value);
