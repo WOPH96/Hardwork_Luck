cecb15128bfa35398db4a0d54a4fc011d377862b
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-16 10:36:28 +0900

    Title: 최대 동전 거슬러주기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250516/최대 동전 거슬러주기/max-coin-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAX_M 10001
+#define MAX_N 101
+
+int coin[MAX_N];
+
+#define max(a,b) ((a>b)?a:b)
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &coin[i]);
+    }
+    
+    // Please write your code here.
+    
+    int dp[MAX_M] = {0};
+
+    for(int i = 1; i<=m; i++)
+    {
+        dp[i] = -1;
+    }
+
+    for(int i = 1; i<=m; i++)
+    {
+        for(int j = 0; j< n; j++)
+        {
+            if(i >= coin[j])
+                dp[i] = max(dp[i],dp[i-coin[j]]+1); 
+        }
+    }
+
+    // for(int i =0; i<=m ;i++)
+    // {  
+    //     printf("%d ",dp[i]);
+    // }
+
+    if(dp[m] == -1)
+        printf("-1");
+    else
+        printf("%d",dp[m]);
+    return 0;
+}
