309e0b33cef3b4afcbed70fec9645b094f5dffcb
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-22 11:17:36 +0900

    Title: 최소 차 분할, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250522/최소 차 분할/minimum-diff-partition.c
+++ 250522/최소 차 분할/minimum-diff-partition.c
@@ -8,14 +8,29 @@ int min_val = 0x7fff0000;
 #define abs(a) ((a<0)?-a:a) 
 #define min(a,b) ((a<b)?a:b)
 
+int dp[101];
+
+void init_dp()
+{
+    for(int i =0 ;i<=n; i++)
+    {
+        dp[i] = 0x7fff0000;
+    }
+}
+
 void backtrack(int idx, int sub_sum)
 {
-    if(abs((sum-sub_sum*2))<min_val)
+    int gap = abs((sum-sub_sum*2));
+    if(gap<min_val)
     {
-        min_val = abs((sum-sub_sum*2));
+        min_val = gap;
         // printf("min_val %d\n",min_val);
     }
     if(idx>=n) return;
+
+    if(dp[idx] < gap) return;
+    dp[idx] = gap;
+
     //선택x
     backtrack(idx+1,sub_sum);
 
@@ -32,6 +47,7 @@ int main() {
     }
     
     // Please write your code here.
+    init_dp();
     backtrack(0,0);
 
     printf("%d",min_val);
