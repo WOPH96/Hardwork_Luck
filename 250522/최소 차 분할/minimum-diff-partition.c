f4f1cbd7163110550ca040525e6628b0b26d7320
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-22 14:36:00 +0900

    Title: 최소 차 분할, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250522/최소 차 분할/minimum-diff-partition.c
+++ 250522/최소 차 분할/minimum-diff-partition.c
@@ -8,7 +8,8 @@ int min_val = 0x7fff0000;
 #define abs(a) ((a<0)?-a:a) 
 #define min(a,b) ((a<b)?a:b)
 
-int dp[101];
+#define MAX_M 100*1000
+int dp[MAX_M];
 
 void init_dp()
 {
@@ -18,26 +19,24 @@ void init_dp()
     }
 }
 
-void backtrack(int idx, int sub_sum)
-{
-    int gap = abs((sum-sub_sum*2));
-    if(gap<min_val)
-    {
-        min_val = gap;
-        // printf("min_val %d\n",min_val);
-    }
-    if(idx>=n) return;
+// void backtrack(int idx, int sub_sum)
+// {
+//     int gap = abs((sum-sub_sum*2));
+//     if(gap<min_val)
+//     {
+//         min_val = gap;
+//         // printf("min_val %d\n",min_val);
+//     }
+//     if(idx>=n) return;
 
-    if(dp[idx] < gap) return;
-    dp[idx] = gap;
+    
+//     //선택x
+//     backtrack(idx+1,sub_sum);
 
-    //선택x
-    backtrack(idx+1,sub_sum);
+//     //선택o
+//     backtrack(idx+1,sub_sum + arr[idx]);
 
-    //선택o
-    backtrack(idx+1,sub_sum + arr[idx]);
-
-}
+// }
 
 int main() {
     scanf("%d", &n);
@@ -47,9 +46,26 @@ int main() {
     }
     
     // Please write your code here.
-    init_dp();
-    backtrack(0,0);
+    // init_dp();
+    // backtrack(0,0);
 
+    dp[0] = 1;
+    for(int i = 0 ; i< n; i++)
+    {
+        for(int s = sum; s>=arr[i]; s--)
+        {
+            if(dp[s-arr[i]]) dp[s] = 1; // 뒤에서부터 sum을 누적하면, 하나의 요소만 할수있음
+        }
+    }
+    min_val = sum;
+    for(int i=1; i<sum; i++)
+    {
+        if(dp[i])
+        {
+            int gap = abs((sum - 2*i));
+            min_val = min(min_val,gap);       
+        }
+    }
     printf("%d",min_val);
     return 0;
 }
