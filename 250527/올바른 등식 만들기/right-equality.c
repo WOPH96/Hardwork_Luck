38a02eb252dc6aae00ac154827e54130cb6db81a
Wonphil <73467421+WOPH96@users.noreply.github.com>
2025-05-27 17:01:04 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250527/올바른 등식 만들기/right-equality.c
+++ 250527/올바른 등식 만들기/right-equality.c
@@ -32,11 +32,33 @@ int main() {
         scanf("%d", &a[i]);
         for(int j =0; j<41;j++)
         {
-            dp[i][j]=-1;
+            dp[i][j]=0;
         }
     }
     // Please write your code here.
 
-    printf("%lld",dfs(0,0));
+    // printf("%lld",dfs(0,0));
+
+    dp[0][20] = 1;
+
+    for (int i = 0 ; i <n; i++)
+    {
+        for(int sum = -20; sum<=20; sum++)
+        {
+            if(dp[i][sum+20] >0)
+            {
+                int next1 = sum + a[i];
+                int next2 = sum - a[i];
+                if(-20 <=next1 && next1 <=20)
+                {
+                    dp[i+1][next1+20] += dp[i][sum+20];
+                }
+                if(-20<=next2 && next2<=20)
+                    dp[i+1][next2+20] += dp[i][sum+20];
+            }
+
+        }
+    }
+    printf("%d",dp[n][m+20]);
     return 0;
 }
